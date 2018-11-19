c125194998cb84c5c1ecad665b17ae13f2cc1487
David <david.cotier@student-cs.fr>
2018-11-19 15:56:31 +0100

    modification note_fondamentale

--- Note_fondamentale.py
+++ Note_fondamentale.py
@@ -5,7 +5,7 @@ import math
 import numpy as np
 from matplotlib.pyplot import *
 import scipy.io.wavfile as wave
-from numpy.fft import fft
+from np.fft import fft
 from itertools import chain
 import argparse
 
@@ -45,12 +45,7 @@ def main():
                 freq[k] = 1.0/n*rate*k
             vlines(freq,[0],spectre,'r')
             print("La fréquence fondamentale de ce son est : {} Hz".format(freq[maxi]))
-            xlabel('f (Hz)')
-            ylabel('A')
-            axis([0,0.5*rate,0,1])
-            grid()
-            show()                                              #Trace le sprectre du son (optionnel)
-
+            
         rate,data = wave.read("recoding.wav")    #Lit le son. Conserve les amplitudes dans la variable data
         n = data.size
         duree = 1.0*n/rate
