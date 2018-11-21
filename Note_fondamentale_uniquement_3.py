8f62c20295d6fdf2bbea7aacb4c59f6fcb6f82bf
MonfretAugustin <45000246+MonfretAugustin@users.noreply.github.com>
2018-11-21 16:45:36 +0100

    edit

--- Note_fondamentale_uniquement_3.py
+++ Note_fondamentale_uniquement_3.py
@@ -23,7 +23,7 @@ def determine_note_fondamentale():
         board.button.when_pressed = done.set
 
         fs = 60000
-        duration = 4 # seconds
+        duration = 2 # seconds
         myrecording = sounddevice.rec(int(duration * fs), samplerate=fs, channels=2)
         sounddevice.wait()
         print("done recording")
