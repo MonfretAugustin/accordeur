e2d738e42d95aac54dee3edb092901b1df4b1fd1
David <david.cotier@student-cs.fr>
2018-11-21 11:06:20 +0100

    n-ème modif de Note_fondamentale

--- Note_fondamentale_uniquement.py
+++ Note_fondamentale_uniquement.py
@@ -2,8 +2,7 @@ import argparse
 import time
 import threading
 import math
-import scipy.io.wavfile as wave
-from numpy.fft import fft
+import scipy
 from itertools import chain
 import argparse
 
@@ -29,7 +28,7 @@ def determine_note_fondamentale():
                 time.sleep(0.5)
         record_file(AudioFormat.CD, filename=args.filename, wait=wait, filetype='wav')
 
-        rate,data = wave.read("recoding.wav")    #Lit le son. Conserve les amplitudes dans la variable data
+        rate,data = scipy.io.wavfile.read("recoding.wav")    #Lit le son. Conserve les amplitudes dans la variable data
         n = data.size
         duree = 1.0*n/rate
         a = chain(*data)
@@ -37,7 +36,7 @@ def determine_note_fondamentale():
 
         start = int(0*rate)
         stop = int((0+1)*rate)
-        spectre = math.abs(fft(data[start:stop]))       #Réalise le spectre
+        spectre = math.abs(scipy.fft(data[start:stop]))       #Réalise le spectre
         spectre = spectre/spectre.max()                    #Normalise le spectre par rapport à la fondamentale
         spectre2 = list(chain(spectre))                    #Transforme l'array en liste
         maxi = spectre2.index(max(spectre))                #Trouve l'indice de la fréquence max (fondamentale)
