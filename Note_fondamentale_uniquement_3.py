e41236975f255d97bcd21c4b24566c30170d06bb
MonfretAugustin <45000246+MonfretAugustin@users.noreply.github.com>
2018-11-22 17:03:03 +0100

    edit

--- Note_fondamentale_uniquement_3.py
+++ Note_fondamentale_uniquement_3.py
@@ -18,11 +18,11 @@ def determine_note_fondamentale():
 
     with Board() as board:
         print('Press button to start recording.')
-        board.led.state = Leds.ON
+        board.led.state = Led.ON
         board.button.wait_for_press()
         done = threading.Event()
         board.button.when_pressed = done.set
-        board.led.state = Leds.OFF
+        board.led.state = Led.OFF
         fs = 60000
         duration = 2 # seconds
         myrecording = sounddevice.rec(int(duration * fs), samplerate=fs, channels=2)
