37bbc8bcac94907bcc26fb2370460a9165fc33cc
MonfretAugustin <45000246+MonfretAugustin@users.noreply.github.com>
2018-11-22 17:09:41 +0100

    edit

--- Note_fondamentale_uniquement_3.py
+++ Note_fondamentale_uniquement_3.py
@@ -70,7 +70,7 @@ def determine_note_fondamentale():
                 spectre2[j]=0
 
         print(freq)
-        return(freq[0])
+        return(2*freq[0])
 
 if __name__=='__main__':
     determine_note_fondamentale()
