e2919bde0269edca8319f093c046b1bf1d15be7c
David <david.cotier@student-cs.fr>
2018-11-23 09:44:25 +0100

    modification de commentaires

--- Accorder_guitare3.py
+++ Accorder_guitare3.py
@@ -11,15 +11,15 @@ import sys
 
 Liste_frequences=[(1,329.6,"mi aigu"),(2,246.9,"si"),(3,196,"sol"),(4,146.8,"ré"),(5,110,"la"),(6,82.4,"mi grave")]  # Liste telle que [(numéro de la corde, fréquence associée en Hz)]  numéro 1 = plus fine, 6 plus grosse
 
-def trouve_freq_souhaitee(frequence):     # trouve la fréquence à obtenir (la plus proche de celle jouée)
+def trouve_freq_souhaitee(frequence):      # trouve la fréquence à obtenir (la plus proche de celle jouée)
     k=0
-    while k < 6 and frequence < Liste_frequences[k][1] :   # Parcourt liste afin de trouver la fréquence la plus proche
+    while k < 6 and frequence < Liste_frequences[k][1] :         # Parcourt liste afin de trouver la fréquence la plus proche
         k+=1
-    if k==0:
+    if k==0:             # Cas extrême où la fréquence est supérieure à toutes
         tts.say('La corde détectée est {}'.format(Liste_frequences[0][2]), lang='fr-FR')
         print("la corde choisie est: ",Liste_frequences[0][0])
         return Liste_frequences[0]
-    elif k==6:
+    elif k==6:            # Cas extrême où la fréquence est inférieure à toutes
         tts.say('La corde détectée est {}'.format(Liste_frequences[5][2]), lang='fr-FR')
         print("la corde choisie est : ",Liste_frequences[5][0])
         return Liste_frequences[5]
@@ -35,7 +35,7 @@ def trouve_freq_souhaitee(frequence):     # trouve la fréquence à obtenir (la 
             print("la corde choisie est : ",Liste_frequences[k][0])
             return Liste_frequences[k]
 
-def minimum(tableau):
+def minimum(tableau):    # fonction renvoyant le minimum d'un tableau
         min_=tableau[0]
         k_min=0
         for k in range(len(tableau)):
@@ -44,22 +44,21 @@ def minimum(tableau):
                 k_min=k
         return min_,k_min
 
-def ecart_avec_objectif(f_fond,f_ref):    # renvoie l'écart sous la forme (valeur de l'écart, -1 ou 1)  -1 pour diminuer, 1 pour augmenter
+def ecart_avec_objectif(f_fond,f_ref):       ### renvoie l'écart entre la fréquence fondamentale obtenue et celle souhaitée
     ecart = f_fond - f_ref
     return ecart
 
-def reponse_bouton(est_juste,ecart):
+def reponse_bouton(est_juste,ecart):         ### réponse donnée par la couleur du bouton et la fréquence du clignotement
     with Leds() as leds :
         if est_juste :
-            leds.update(Leds.rgb_on(Color.GREEN))           # Vert fixe pendant 3 secondes si fréquence atteinte
+            leds.update(Leds.rgb_on(Color.GREEN))        # Vert fixe pendant 3 secondes si fréquence atteinte
             time.sleep(3)
             print ('Corde accordée')
-            tts.say('Corde accordée',lang='fr-FR')          ####### Dire la phrase en plus #######
+            tts.say('Corde accordée',lang='fr-FR')
         else :
             period = 10*abs(ecart)
-            leds.pattern=Pattern.blink(period)          # donne fréquence de pulsation
+            leds.pattern=Pattern.blink(period)   # donne fréquence de pulsation
             print("TOURNER LA CHEVILLE")
-                 ####### Dire la phrase #######
             if ecart>0 :
                 tts.say('Tendre la corde', lang='fr-FR')
                 leds.update(Leds.rgb_pattern(Color.BLUE))       #Clignotement bleu pour augmenter pendant 5 secondes
@@ -71,7 +70,7 @@ def reponse_bouton(est_juste,ecart):
                 time.sleep(5)
 
 
-def accord_de_la_corde () :
+def accord_de_la_corde () :        ### A l'aide des fonctions précédentes, permet d'accorder une corde
     est_juste=False
     while est_juste==False:
         f_fond = Nfu.determine_note_fondamentale()
@@ -83,11 +82,11 @@ def accord_de_la_corde () :
         reponse_bouton(est_juste,ecart)
 
 
-def test_justesse(ecart,f_ref):
+def test_justesse(ecart,f_ref):     ### fonction permettant de considérer que la corde est bien ajustée ou non
         rapport=(f_ref+abs(ecart))/f_ref
         return (rapport<1.02)
     
-def accord_de_la_guitare():
+def accord_de_la_guitare():     ### Réalise l'accord de toutes les cordes de la guitare
     with Board() as board:
         tts.say("commencer à accorder la guitare", lang='fr-FR')
         print("commencer à accorder la guitare")
--- Note_fondamentale_uniquement_3.py
+++ Note_fondamentale_uniquement_3.py
@@ -11,7 +11,7 @@ from aiy.leds import (Leds, Pattern, PrivacyLed, RgbLeds, Color)
 from aiy.board import Board, Led
 from aiy.voice.audio import AudioFormat, play_wav, record_file, Recorder
 
-
+##### Une seule fonction dans ce module qui a pour objectif d'analyser le son émis suite à l'appui sur le bouton et de renvoyer sa fréquence fondamentale #####
 
 def determine_note_fondamentale():
     parser = argparse.ArgumentParser()
@@ -28,11 +28,11 @@ def determine_note_fondamentale():
         board.button.when_pressed = done.set
         board.led.state = Led.OFF
         fs = 60000
-        duration = 2 # seconds
+        duration = 2        # seconds
         myrecording = sounddevice.rec(int(duration * fs), samplerate=fs, channels=2)
         sounddevice.wait()
         print("done recording")
-        myrecording2 = list(chain(*myrecording)) #On augmente de façon virtuel la durée du signal pour augmenter la précision de la fft
+        myrecording2 = list(chain(*myrecording))        # On augmente de façon virtuelle la durée du signal pour augmenter la précision de la fft
         
 
         """def readwave(filename):
@@ -60,15 +60,15 @@ def determine_note_fondamentale():
 
         start = int(0*rate)
         stop = int((0+1)*rate)
-        spectre = abs(fft(data[start:stop]))       #Réalise le spectre
-        spectre = spectre/spectre.max()                    #Normalise le spectre par rapport à la fondamentale
-        spectre2 = list(chain(spectre))                    #Transforme l'array en liste
+        spectre = abs(fft(data[start:stop]))               # Réalise le spectre
+        spectre = spectre/spectre.max()                    # Normalise le spectre par rapport à la fondamentale
+        spectre2 = list(chain(spectre))                    # Transforme l'array en liste
         freq = []
         n=len(spectre2)
-        spectre2 = spectre2[0:20000]		#On coupe les fréquences supérieurs à 20000
+        spectre2 = spectre2[0:20000]		                # On coupe les fréquences supérieurs à 20000
         for _ in range(3):
 
-            maxi = spectre2.index(max(spectre2))                #Trouve l'indice de la fréquence max (fondamentale)
+            maxi = spectre2.index(max(spectre2))           # Trouve l'indice de la fréquence max (fondamentale)
             freq.append(1.0/n*rate*maxi)
             for j in range(maxi-60,maxi+60):
                 spectre2[j]=0
